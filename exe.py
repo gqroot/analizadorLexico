7c6891e981782f8cab876bab80aa5ffa20129509
Guido Quezada <gqrott@gmail.com>
2019-06-05 09:27:04 -0500

    Fixes

--- exe.py
+++ exe.py
@@ -1,32 +1,56 @@
-import re # Importamos el modulo regex
-
 # coding: utf-8
-global listValues
-global symbols
-global states
+
 global data
-listValues = []
-symbols = []
-states = 0
 data = []
+global states
+states = []
+global symbols
+symbols = []
+global listValues
+listValues = []
 global transitions
 transitions = []
-global expresion
-expresion = ''
+global finalStates
+finalStates = []
 
-def read_file(name):
-    try:
-        x = open(name, 'r')
-        return x
-    except:
-        return('Something went wrong when reading to the file')
-        x.close()
+def automaton(word, current, response=None):
+    if len(word)>0:
+        for char in word:
+            if not char in symbols:
+                return None
 
-def set_values(values):
-    for v in values:
-        listValues.append(v)
+        for state in states[current][symbols.index(word[0])]:
+            current = state
+            if len(word) == 1:
+                if current in finalStates:
+                    response = True
+                else:
+                    continue
+            else:
+                response = automaton(word[1:], current, response)                   
+    return response
 
-def get_symbol(v, index):
+#Fix please
+def reorder():
+    state = 0
+    while state <= finalStates[0]:
+        a = []
+        for symbol in symbols:
+            b = []
+            for transition in transitions:
+                if symbol == transition[1] and state == transition[0]:
+                    b.append(transition[2])
+            a.append(b)
+        states.append(a)
+        state += 1
+
+def getInitial(v):
+    if (v.isdigit()):
+        return int(v)
+    else:
+        return None
+
+def getSymbol(v, index):
     i = 0
     if(v.isdigit()):
         for n in listValues:
@@ -35,13 +59,7 @@ def get_symbol(v, index):
             i += 1    
     return None
 
-def get_initial(v):
-    if (v.isdigit()):
-        return int(v)
-    else:
-        return None
-
-def set_data(values):
+def setData(values):
     inital_state = values[0:1]
     end_state = values[2: len(values)-1]
     index = 1
@@ -50,121 +68,73 @@ def set_data(values):
         data_state = []
         if len(v) > 0:
             data_state.append(int(inital_state))
-            if(get_symbol(v, index)):
-                data_state.append(get_symbol(v, index))
-                data_state.append(get_initial(v))
+            if(getSymbol(v, index)):
+                data_state.append(getSymbol(v, index))
+                data_state.append(getInitial(v))
         if(len(data_state) == 3):
             data.append(data_state)
 
-def reorder(transitions, symbols):
-    a=[]
-    states=get_states()
-    count=0
-    while count <= states:
-        b=[]
-        for symbol in symbols:
-            c=[]
-            for transition  in transitions:
-                if symbol == transition[1] and count == transition[0]:
-                    c.append(transition[2])
-            b.append(c)
-        a.append(b)
-        count+=1
-    return a
+def setValues(line):
+    for idy, char in enumerate(line):
+        listValues.append(char)
+        if idy > 1 and idy % 2 == 0 :
+            symbols.append(char)
 
-def get_data():
+def getTransitionTable():
     try:
-        f = read_file('data.csv')
-        index = 0
-        for linea in f:
-            if len(linea) > 1:
-                if(index == 0 ):
-                    set_values(linea)
+        csv = readFile('data.csv')
+        for idx, line in enumerate(csv):
+            if len(line) > 1:
+                if idx == 0 :
+                    setValues(line)
                 else:
-                    set_data(linea)
-            index += 1
-        transitions = reorder(remove_duplicates(data), get_symbols())
-        f.close()
-        return transitions
+                    setData(line)
+        for item in data:
+            if item not in transitions:
+                transitions.append(item)
+
+        with open('data.csv') as myfile:
+            finalStates.append(int(list(myfile)[-1][0]))
+
+        reorder()
+        csv.close()
     except Exception as inst:
         raise Exception(inst)
     finally:
-        f.close()
+        csv.close()
 
-def get_symbols():
+def readFile(name):
     try:
-        f = read_file('data.csv')
-        for idx, linea in enumerate(f):
-            if idx == 0:
-                for idy, v in enumerate(linea):
-                    if idy > 1 and idy%2 == 0 :
-                        symbols.append(v)
-        f.close()
-        return symbols
-    except Exception as inst:
-        raise Exception(inst)
-    finally:
-        f.close()
-
-def get_states():
-    try:
-        f = read_file('data.csv')
-        idx=-0
-        for linea in f:
-            idx+=1
-        f.close()
-        return idx-2
-    except Exception as inst:
-        raise Exception(inst)
-    finally:
-        f.close()
-
-def remove_duplicates(lt):
-    new_k = []
-    for elem in lt:
-        if elem not in new_k:
-            new_k.append(elem)
-    k = new_k
-    return new_k
-
-def automaton(word, current, transitions, symbols,resultado=None):
-    if len(word)>0:
-        for char in word:
-            if not char in symbols:
-                return None
-
-        for next in transitions[current][symbols.index(word[0])]:
-            current = next
-            if len(word) == 1:
-                if current == get_states():
-                    resultado = True
-                else:
-                    continue
-            else:
-                resultado = automaton(word[1:],current, transitions, symbols, resultado)                   
-    return resultado
+        x = open(name, 'r')
+        return x
+    except:
+        return('Something went wrong when reading to the file')
+        x.close()
         
 def main():
     while(True):
         print('\n<<<===== Choose option =====>>>\n1.Automaton\n2.Exit')
-        op = input()
-        if op == 1:
+        option = input()
+        if option == 1:
             try:
-                txt = read_file('data.txt')
-                transitions = get_data() 
-                # print(transitions, symbols)
+                txt = readFile('data.txt')
+                getTransitionTable() 
+
                 for idx, word in enumerate(txt):
-                    if(automaton(word.split('\n')[0],0,transitions, symbols) == True):
-                        # print('Valid word')
-                        pass
-                    else:
-                        print('Invalid word line: '+ str(idx))
+                    response = automaton(word.split('\n')[0], 0)
+                    if( response == None):
+                        print('Invalid word line: ' + str(idx + 1))
+
                 txt.close()
             except Exception as e:
                 print(e)
             finally:
                 txt.close()
-        if op == 2:
+        if option == 2:
             break
+        try:
+            input()
+        except SyntaxError:
+            pass
 main()
 
